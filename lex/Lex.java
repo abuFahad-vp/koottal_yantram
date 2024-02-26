7506b828630d07b1aa859fd6d0bfe988bbef2085
abuFahad007 <fahadabu30c@gmail.com>
2024-02-26 22:15:57 +0530

    Lexing and event loop completed.

--- lex/Lex.java
+++ lex/Lex.java
@@ -6,12 +6,6 @@ import java.util.Stack;
 import java.util.Arrays;
 import java.util.List;
 
-/*
- * numbers only
- * words only
- * symbols only
- */
-
 public class Lex {
 
   static String[] allowed_tokens = {"+","-","*","/",")","("};
